2b323c1e797e672a1855326702c0daef1274af2f
Samueltse37 <samuelt37@gmail.com>
2026-08-12 23:25:53 -0700

    added sidebar with api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Layout } from "./components/layout/Layout";
+import { MemoryPage } from "./pages/MemoryPage";
+// import { BookPage } from "./pages/BookPage";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <h1>Bible Memory</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<MemoryPage />} />
+            {/* <Route path="/book/:bookName" element={<BookPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
-export default App
+export default App;
